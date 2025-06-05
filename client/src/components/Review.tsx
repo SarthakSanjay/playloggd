17451ce222a31242d3ec7f5a72f99694a119729a
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2025-06-05 23:07:20 +0530

    redesign review component

--- client/src/components/Review.tsx
+++ client/src/components/Review.tsx
@@ -1,9 +1,10 @@
-import { Dot, Star } from "lucide-react";
+import { ChevronDown, Heart, MessageSquare, Star } from "lucide-react";
 import { GameCard } from "./GameCard";
+import { Button } from "./ui/button";
 
 export default function Review() {
   return (
-    <div className="h-64 w-1/2 border border-white flex">
+    <div className="h-64 w-1/2  border-b border-greyblue-200 hover:bg-gradient-to-t from-greyblue-200 to-greyblue flex rounded-b-sm ">
       <GameCard size={"md"}>
         <img
           className="h-full w-full rounded-sm object-cover object-center"
@@ -12,7 +13,7 @@ export default function Review() {
           }
         />
       </GameCard>
-      <div className="h-full w-[88%] border border-white pl-4">
+      <div className="h-full w-[88%] pl-4">
         <div className="flex items-center gap-2 h-7 w-full">
           <h1 className="text-white/80 font-medium text-lg h-full">Username</h1>
           <div className="flex items-end gap-1">
@@ -24,7 +25,7 @@ export default function Review() {
             </span>
           </div>
         </div>
-        <div className="h-5 w-full flex gap-1 items-center">
+        <div className="h-5 w-full flex gap-2 items-center">
           <div className="h-full w-max flex">
             {[1, 2, 3, 4, 5].map((i) => {
               return <Star key={i} fill="grey" strokeWidth={0} size={18} />;
@@ -37,6 +38,29 @@ export default function Review() {
               on <span className="text-white">Mac</span>
             </div>
           </div>
+        </div>
+        <div className="h-[60%] w-full text-white">
+          The Witcher IV is an upcoming action role-playing game developed by CD
+          Projekt Red and published by CD Projekt. It is the planned first
+          installment of a new trilogy in The Witcher series, which is based on
+          the book series of the same name by Andrzej Sapkowski.
+        </div>
+        <div className="h-10 w-full  flex items-center gap-2 ">
+          <div className="w-[20%]  text-white flex items-center gap-1">
+            <Heart fill="grey" strokeWidth={0} size={18} /> 27 Likes
+          </div>
+          <div className="flex items-center gap-1 text-white">
+            <MessageSquare fill="white" strokeWidth={0} size={18} />
+            <span>0</span>
+          </div>
+          <Button className="text-lightblue font-light text-base bg-greyblue-200 h-8 ">
+            Open review
+          </Button>
+
+          <Button className="bg-transparent h-8 ml-auto mr-2">
+            <ChevronDown size={18} />
+            <span>Expand</span>
+          </Button>
         </div>
       </div>
     </div>
