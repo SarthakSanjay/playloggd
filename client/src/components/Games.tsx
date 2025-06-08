9955048d817d4f9fce4a5dbe17fddebca8e08097
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2025-06-08 23:32:15 +0530

    refactor: displayed games and add pagination tab

--- client/src/components/Games.tsx
+++ client/src/components/Games.tsx
@@ -1,25 +1,47 @@
+"use client";
 import { ChevronDown } from "lucide-react";
 import { Button } from "./ui/button";
 import { SortBy } from "./SortBy";
+import { GameCard } from "./GameCard";
+import { PaginationTab } from "./PaginationTab";
+import { FilterMenu } from "./FilterMenu";
 
 export default function Games() {
   return (
-    <div className="">
+    <div className="h-max w-full overflow-y-scroll space-y-2">
       <FilterBar />
+      <div className="h-max w-full flex flex-wrap gap-y-5 gap-x-5 justify-center border-b border-greyblue-200 pb-4">
+        {Array(60)
+          .fill("")
+          .map((_, idx) => {
+            return (
+              <GameCard key={idx} size={"xxl"} className="border-none">
+                <img
+                  className="h-full w-full rounded-sm"
+                  src={
+                    idx % 2 === 0
+                      ? "https://images.igdb.com/igdb/image/upload/t_cover_big/co1r7f.jpg"
+                      : "https://images.igdb.com/igdb/image/upload/t_cover_big/co5ziw.jpg"
+                  }
+                />
+              </GameCard>
+            );
+          })}
+      </div>
+      <PaginationTab />
     </div>
   );
 }
 
 function FilterBar() {
   return (
-    <div className="h-10 w-full bg-greyblue-200 flex items-center justify-between text-white">
-      <div className="h-full w-max">12345 Games</div>
-      <div className="h-full w-1/2">
-        <Button>Apply Filter</Button>
-        <Button>
+    <div className="h-10 w-full rounded-sm bg-greyblue-200 flex items-center justify-between text-white px-1 ">
+      <div className="h-full w-max flex items-center text-sm">12345 Games</div>
+      <div className="h-full w-1/2 flex items-center justify-end gap-1">
+        <FilterMenu />
+        <Button className="h-8 text-sm">
           Display <ChevronDown />
         </Button>
-        <div className="flex gap-2">Sort By </div>
         <SortBy />
       </div>
     </div>
